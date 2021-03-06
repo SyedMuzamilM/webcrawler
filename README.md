e96e1a3ae660fc9b2e22533e3f468441d4a103c6
Syed Muzamil <smmhd121@gmail.com>
2021-03-07 00:38:33 +0530

    Added functionality to get the images from dynamic website

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Simple Webcrawler
 
-This is the basic version of the web crawler where i have created a simple website inside the html folder. And which i ran by the live server. And then i tried to crawl all the webpages from that website. And then try to find the image in one of the webpages. Then it downloaded the image inside the images folder of the root directory.
+
---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import path = require('path')
+import parse = require("url-parse");
+
+// stores the crawled webpages
+const seenUrls = {};
+
+// used to get the full url.
+const getUrl = (link, host, protocol) => {
+    if (link.includes("http" || "https")) {
+        return link;
+    } else if (link.startsWith("/")) {
+        return `${protocol}//${host}${link}`;
+    } else {
+        return `${protocol}//${host}/${link}`;
+    }
+};
+
+const crawl = async ({ url, ignore }) => {
+    if (seenUrls[url]) return;
+
+    console.log("crawling", url);
+    seenUrls[url] = true;
+
+    const { host, protocol } = parse(url)
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    const $ = cheerio.load(html);
+    const links = $("a")
+        .map((i, link) => link.attribs.href)
+        .get();
+
+    const imageUrls = $('img')
+        .map((i, link) => link.attribs.src)
+        .get()
+
+
+    imageUrls.forEach(imageUrl => {
+        fetch(getUrl(imageUrl, host, protocol)).then(response => {
+            const filename = path.basename(imageUrl)
+            const dest = fs.createWriteStream(`images/${filename}`)
+            response.body.pipe(dest)
+        })
+
+
+        // For each link it will repeate the same process
+        links
+            .filter(link => link.includes(host) && !link.includes(ignore))
+            .forEach((link) => {
+                crawl({
+                    url: getUrl(link, host, protocol),
+                    ignore
+                });
+            });
+    });
+}
+crawl({
+    url: "https://mozilla.org/",
+    ignore: "/search",
+})
